ee307780ddea14059f987fa24be2fea802e9af1c
mors-e <mors@nemors.ru>
2023-05-17 13:14:20 +0300

    change detail page (header link to home)

--- src/components/DetailModel/index.jsx
+++ src/components/DetailModel/index.jsx
@@ -1,14 +1,14 @@
 import styles from "./detail_model.module.css";
-import {useState} from "react";
+import React, {useState} from "react";
 import {Link, useRouteLoaderData} from "react-router-dom";
 import Header from "../Header";
 
-const handleEditClick = () => {
+const handleEditClick = async () => {
     console.log('Кнопка "Редактировать" была нажата!');
     // код редактирования
 };
 
-const handleDeleteClick = () => {
+const handleDeleteClick = async () => {
     console.log('Кнопка "Удалить" была нажата!');
     // код удаления
 };
@@ -20,36 +20,40 @@ const formatDate = dateStr => {
 
 export default function DetailModel() {
     const [modelData] = useState(useRouteLoaderData("modelDetail"));
-
     const formattedDate = formatDate(modelData.time_create);
+
     return (
         <>
-            <Header />
-            <section className={styles.detailPage}>
-                <Link to="/">Назад в список</Link>
-                <h1>Модель "{modelData.name_model}"</h1>
-                <div className={styles.modelInformation}>
-                    <div className={styles.modelContainer}>
-                        <canvas className={styles.modelCanvas}></canvas>
+        <Header/>
+        <main>
+            <div className="container">
+                <section className={styles.detailPage}>
+                    <Link to="/">Назад в список</Link>
+                    <h1>Модель "{modelData.name_model}"</h1>
+                    <div className={styles.modelInformation}>
+                        <div className={styles.modelContainer}>
+                            <canvas className={styles.modelCanvas}></canvas>
+                        </div>
+                        <p><strong>Название: </strong>{modelData.name_model}</p>
+                        <p><strong>Тип модели: </strong>{modelData.type}</p>
+                        <p className={styles.description}><strong>Описание: </strong>{modelData.description}</p>
+                        <p><strong>Дата создания: </strong>{formattedDate}</p>
+                        <button onClick={handleEditClick} className={styles.edit}>Редактировать</button>
+                        <button onClick={handleDeleteClick} className={styles.delete}>Удалить</button>
                     </div>
-                    <p><strong>Название: </strong>{modelData.name_model}</p>
-                    <p><strong>Тип модели: </strong>{modelData.type}</p>
-                    <p className={styles.description}><strong>Описание: </strong>{modelData.description}</p>
-                    <p><strong>Дата создания: </strong>{formattedDate}</p>
-                    <button onClick={handleEditClick} className={styles.edit}>Редактировать</button>
-                    <button onClick={handleDeleteClick} className={styles.delete}>Удалить</button>
-                </div>
-                <div>
-                    <h2>Комментарии</h2>
-                    <div className={styles.commentList}>
-                        {modelData.comments.map((comment, index) => (
-                            <div key={index} className={styles.commentItem}>
-                                {comment.text}
-                            </div>
-                        ))}
+                    <div>
+                        <h2>Комментарии</h2>
+                        <div className={styles.commentList}>
+                            {modelData.comments.map((comment, index) => (
+                                <div key={index} className={styles.commentItem}>
+                                    {comment.text}
+                                </div>
+                            ))}
+                        </div>
                     </div>
-                </div>
-            </section>
+                </section>
+            </div>
+        </main>
         </>
     );
 }
