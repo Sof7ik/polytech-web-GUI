c1075884eaf04bfe3b750e3b94755982b09c0ce6
mors-e <mors@nemors.ru>
2023-05-17 12:51:46 +0300

    header link

--- src/components/DetailModel/index.jsx
+++ src/components/DetailModel/index.jsx
@@ -23,44 +23,33 @@ export default function DetailModel() {
 
     const formattedDate = formatDate(modelData.time_create);
     return (
-
-        <section className={styles.detailPage}>
-
-            <h1>Модель "{modelData.name_model}"</h1>
-            <div className={styles.modelInformation}>
-                <div className={styles.modelContainer}>
-                    <canvas className={styles.modelCanvas}></canvas>
-                </div>
-                <p><strong>Название: </strong>{modelData.name_model}</p>
-                <p><strong>Тип модели: </strong>{modelData.type}</p>
-                <p className={styles.description}><strong>Описание: </strong>{modelData.description}</p>
-                <p><strong>Дата создания: </strong>{formattedDate}</p>
-                <button onClick={handleEditClick} className={styles.edit}>Редактировать</button>
-                <button onClick={handleDeleteClick} className={styles.delete}>Удалить</button>
-            </div>
-            <div>
-                <h2>Комментарии</h2>
-                <div className={styles.commentList}>
-                    {modelData.comments.map((comment, index) => (
-                        <div key={index} className={styles.commentItem}>
-                            {comment.text}
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </section>
-
         <>
             <Header />
-            <main>
-                <div className="container">
-                    <section className={styles.detailPage}>
-                        <Link to="/">Назад в список</Link>
-
-                        <h1>Модель "{modelData.name_model}"</h1>
-                    </section>
+            <section className={styles.detailPage}>
+                <Link to="/">Назад в список</Link>
+                <h1>Модель "{modelData.name_model}"</h1>
+                <div className={styles.modelInformation}>
+                    <div className={styles.modelContainer}>
+                        <canvas className={styles.modelCanvas}></canvas>
+                    </div>
+                    <p><strong>Название: </strong>{modelData.name_model}</p>
+                    <p><strong>Тип модели: </strong>{modelData.type}</p>
+                    <p className={styles.description}><strong>Описание: </strong>{modelData.description}</p>
+                    <p><strong>Дата создания: </strong>{formattedDate}</p>
+                    <button onClick={handleEditClick} className={styles.edit}>Редактировать</button>
+                    <button onClick={handleDeleteClick} className={styles.delete}>Удалить</button>
                 </div>
-            </main>
+                <div>
+                    <h2>Комментарии</h2>
+                    <div className={styles.commentList}>
+                        {modelData.comments.map((comment, index) => (
+                            <div key={index} className={styles.commentItem}>
+                                {comment.text}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
